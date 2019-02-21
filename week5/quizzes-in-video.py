b6de0392d0dc23760dda18079546f66b2f5f1560
rahulxdd <rahulxdd@gmail.com>
2019-02-21 22:02:31 +0530

    Update quizzes-in-video.py

--- week5/quizzes-in-video.py
+++ week5/quizzes-in-video.py
@@ -24,3 +24,20 @@ n
 c
 c
 d
+
+#Select the expression that correctly completes the missing line in function last_vowel.
+
+def last_vowel(s):
+    """(str) -> str
+    Return the last vowel in s if one exists; otherwise, return None.
+    >>> last_vowel("cauliflower")
+    "e"
+    >>> last_vowel("pfft")
+    None
+    """
+    i = len(s) - 1 #this was added
+    while i >= 0:
+         if s[i] in 'aeiouAEIOU':
+             return s[i]
+         i = i - 1
+    return None
